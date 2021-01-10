1dee1cb0df904d73cf6eac598e9fc9b874061d30
Delictum <thedrakoneragoni@gmail.com>
2021-01-10 12:39:18 +0300

    Fixed importing and updated README.md

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,8 @@
 import pytest
 
-from pages.basket_page import BasketPage
-from pages.main_page import MainPage
-from pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
+from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 LINK_MAIN_PAGE = "http://selenium1py.pythonanywhere.com/"
