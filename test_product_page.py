48e45d1a801118cde1d74aa2e2040515a49433d4
Delictum <thedrakoneragoni@gmail.com>
2021-01-10 13:07:41 +0300

    Reverted import and updated README.md: added description of import problem during cloning and solution

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,9 @@
 import pytest
 from faker import Faker
 
-from .pages.basket_page import BasketPage
-from .pages.login_page import LoginPage
-from .pages.product_page import ProductPage
+from pages.basket_page import BasketPage
+from pages.login_page import LoginPage
+from pages.product_page import ProductPage
 
 
 LINK_CODERS_AT_WORK = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
